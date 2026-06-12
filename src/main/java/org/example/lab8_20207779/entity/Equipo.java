c9047f7f22ef6b75412f9a743675bf66f30562d2
paolaflores0205 <a20222923@pucp.edu.pe>
2026-06-11 20:33:26 -0500

    cambio

--- src/main/java/org/example/lab8_20207779/entity/Equipo.java
+++ src/main/java/org/example/lab8_20207779/entity/Equipo.java
@@ -1,4 +1,22 @@
 package org.example.lab8_20207779.entity;
 
 public class Equipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EquipoID")
+    private int id;
+    @Column(nullable = false)
+    private String lastname;
+    @Column(nullable = false)
+    private String firstname;
+    private String title;
+    private String titleofcourtesy;
+
+    private String address;
+    private String city;
+    private String region;
+    private String postalcode;
+    private String country;
+    private String homephone;
+    private String extension;
 }
