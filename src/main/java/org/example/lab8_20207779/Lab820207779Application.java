dfa2d2a489ee3f6a8a24830ce8be48081006f5e2
paolaflores0205 <a20222923@pucp.edu.pe>
2026-06-11 19:23:54 -0500

    primer commit

---
+++ src/main/java/org/example/lab8_20207779/Lab820207779Application.java
@@ -0,0 +1,13 @@
+package org.example.lab8_20207779;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820207779Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab820207779Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lab8_20207779/Lab820207779ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab8_20207779;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820207779ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
